d7b1fe6eadfac0b3a6e08e81222b00aebd9462af
munigantiarun <arun.muniganti@gmail.com>
2021-03-04 23:40:57 +0530

    Log4j added

--- src/test/java/com/inetBanking/testCases/BaseClass.java
+++ src/test/java/com/inetBanking/testCases/BaseClass.java
@@ -1,5 +1,7 @@
 package com.inetBanking.testCases;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -13,16 +15,21 @@ public class BaseClass {
 	public String password = "apAzymU";
 	public static WebDriver driver;
 	
+	public static Logger logger;
 	@BeforeClass
 	public void setup()
 	{
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
 		driver = new ChromeDriver();
+		
+		logger = Logger.getLogger("ebanking");
+		PropertyConfigurator.configure("Log4j.properties");
 	}
 	
 	@AfterClass
-	public void tearDown()
+	public void tearDown() throws InterruptedException
 	{
+		Thread.sleep(2000);
 		driver.quit();
 	}
 }
--- src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetBanking/testCases/TC_LoginTest_001.java
@@ -10,27 +10,35 @@ public class TC_LoginTest_001 extends BaseClass
 {
 
 	@Test
-	public void loginTest() 
+	public void loginTest() throws InterruptedException 
 	{
 		//first we need to open URL
 		driver.get(baseURL);
-		
+		logger.info("URL is opened");
+		driver.manage().window().maximize();
 		//we have to use page object class, Here below we are passing driver as argument, 
 		//which are pre-defined in pageObject methods
 		
 		LoginPage lp = new LoginPage(driver);  //creating object of LoginPage
-		lp.setUserName(username);    		   //by using the object lp call the methods
+		Thread.sleep(2000);
+		lp.setUserName(username);
+		//by using the object lp call the methods
+		logger.info("Username entered!");
+		Thread.sleep(2000);
 		lp.setPassword(password);
-		
+		logger.info("Password Entered!");
+		Thread.sleep(2000);
 		lp.clickSubmit();
 		
 		if (driver.getTitle().equals("Guru99 Bank Manager HomePage"))
 		{
 		  Assert.assertTrue(true);
+		  logger.info("Login Test passed!");
 		}
 		else
 		{
 			Assert.assertTrue(false);
+			logger.info("Login Test failed");
 		}
 	}
 }
